03a69216019f004dcfc9cce7ca196f04a90e8426
drew ellingson <drew.ellingson@gmail.com>
2022-12-02 00:33:07 -0800

    sick strats for rock paper scissors

---
+++ day_02/soln.py
@@ -0,0 +1,29 @@
+with open("input.txt") as my_file:
+    rounds = [tuple(line.strip().split(" ")) for line in my_file.readlines()]
+
+wins = {"A": "Y", "B": "Z", "C": "X"}
+losses = {"A": "Z", "B": "X", "C": "Y"}
+draws = {"A": "X", "B": "Y", "C": "Z"}
+
+
+def score(round, p2=False):
+
+    if p2:
+        round = (
+            round[0],
+            wins[round[0]]
+            if round[1] == "Z"
+            else draws[round[0]]
+            if round[1] == "Y"
+            else losses[round[0]],
+        )
+
+    letter_score = ord(round[1]) - ord("X") + 1
+    round_score = 3 * (
+        1 + (wins.get(round[0]) == round[1]) - (losses.get(round[0]) == round[1])
+    )
+    return letter_score + round_score
+
+
+print(f"P1 Soln is: {sum(score(round) for round in rounds)}")
+print(f"P2 Soln is: {sum(score(round, p2=True) for round in rounds)}")
