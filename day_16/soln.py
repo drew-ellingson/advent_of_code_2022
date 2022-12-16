8dc4eeb4243b0459490dbc25bc1a65ae5e773601
drew ellingson <drew.ellingson@gmail.com>
2022-12-16 11:13:37 -0800

    wip for graph creation

--- day_16/soln.py
+++ day_16/soln.py
@@ -9,10 +9,34 @@ class Node:
     def __repr__(self):
         return f'valve: {self.label}\nflow rate: {self.flow}\nchildren: {self.children}\n'
 
+class Valves:
+    def __init__(self, nodes):
+        self.nodes = nodes 
+        self.minutes = 30
+        self.curr_path = ['AA']
+    
+    def score(path):
+        pass 
+
+    def get_len_n_paths(self, n):
+        pass 
+
+    def make_best_move(self, n):
+        paths = self.get_len_n_paths(n)
+        best = max(paths, key = lambda p: self.score(p))[0]
+        
+        self.curr_path.append(best)
+        self.minutes -= 1
+
+    def find_max_flux(self, n):
+        while self.minutes > 0:
+            self.make_best_move(n)
+        return self.score(self.curr_path)
+
 def parse(line):
+    label = line[6:8]
     flow, children = line.split(';')
-    label = flow[6:8]
-    flow = re.sub('[^0-9]', '', flow)
+    flow = int(re.sub('[^0-9]', '', flow))
     children = re.sub(' tunnel[s]* lead[s]* to valve[s]*', '', children)
     children = children.strip().split(',')
     return label, flow, children 
